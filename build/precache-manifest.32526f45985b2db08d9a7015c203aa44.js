5434336a85699289a8c7d356c84ffe2a867f1eb1
Leonardo <leosul@gmail.com>
2020-01-08 14:20:33 +0000

    Configurations to run build

---
+++ build/precache-manifest.32526f45985b2db08d9a7015c203aa44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9713b0d150f9cc6ddaeb87ff09a08be6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dc6b633da36b3a4bfaf",
+    "url": "/static/css/main.c23e647e.chunk.css"
+  },
+  {
+    "revision": "cea44d921131452530c5",
+    "url": "/static/js/2.2a458a4c.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.2a458a4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3dc6b633da36b3a4bfaf",
+    "url": "/static/js/main.da705656.chunk.js"
+  },
+  {
+    "revision": "a16e6c4546a7c4446699",
+    "url": "/static/js/runtime-main.d94bf50c.js"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -15,4 +15,11 @@ function errorHandler(err, req, res, next) {
   res.status(500).json({ internal_server_error: err })
 }
 
+const buildPath = path.join(__dirname, '..', 'build')
+app.use(express.static(buildPath))
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(buildPath, 'index.html'))
+})
+
 module.exports = app
