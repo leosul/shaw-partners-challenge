b417a601837b80b11bc01841de14fc5e3d96f3b0
Leonardo <leosul@gmail.com>
2020-01-08 14:28:47 +0000

    Fix module path

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const routes = require('./routes')
+const path = require('path')
 
 const app = express()
 
